54a466dfa184ae8a8b92d25bd86e8725b0b88641
Anton <fpbidni@gmail.com>
2024-11-26 21:59:29 +0100

    put assets on the map =)

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,6 +1,7 @@
 package main;
 
 import entity.Player;
+import object.SuperObject;
 import tile.TileManager;
 
 import javax.swing.*;
@@ -32,7 +33,10 @@ public class GamePanel extends JPanel implements  Runnable {
     KeyHandler keyH = new KeyHandler();
     Thread gameThread;
     public CollisionChecker cChecker = new CollisionChecker(this);
+    public AssetSetter aSetter = new AssetSetter(this);
     public Player player = new Player(this, keyH);
+    public SuperObject obj[] = new SuperObject[10];
+
 
 
 
@@ -43,6 +47,11 @@ public class GamePanel extends JPanel implements  Runnable {
         this.addKeyListener(keyH);
         this.setFocusable(true);
 
+
+    }
+
+    public void setupGame() {
+        aSetter.setObject();
     }
 
     public void startGameThread() {
@@ -90,8 +99,15 @@ public class GamePanel extends JPanel implements  Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
+        //TILE
         tileM.draw(g2);
 
+        //OBJECT
+        for(int i = 0; i < obj.length; i++) {
+            if(obj[i] != null) obj[i].draw(g2, this);
+        }
+
+        // PLAYER
         player.draw(g2);
         g2.dispose();
     }
