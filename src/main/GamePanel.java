48f93b751a4889679cdf579d3d4e073f65e79a6d
Anton <fpbidni@gmail.com>
2024-11-26 16:44:16 +0100

    Game setup

---
+++ src/main/GamePanel.java
@@ -0,0 +1,33 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GamePanel extends JPanel implements  Runnable {
+    final int originalTieSize = 16;
+    final int scale = 3;
+
+    final int tileSize = originalTieSize * scale;
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    Thread gameThread;
+
+    public GamePanel () {
+        this.setPreferredSize(new Dimension(screenWidth, screenHeight));
+        this.setBackground(Color.black);
+        this.setDoubleBuffered(true);
+    }
+
+    public void startGameThread() {
+        gameThread = new Thread(this);
+        gameThread.start();
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
