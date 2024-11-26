1811099876f945b4015fa21909775e6b8c02e92a
Anton <fpbidni@gmail.com>
2024-11-26 18:10:26 +0100

    did man walking

---
+++ src/entity/Player.java
@@ -0,0 +1,96 @@
+package entity;
+
+import main.GamePanel;
+import main.KeyHandler;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Player extends Entity {
+
+    private GamePanel gp;
+    private KeyHandler keyH;
+    private BufferedImage up1, up2, down1, down2, left1, left2, right1, right2;
+    private int spriteCounter = 0; // Added sprite counter for animation
+    private int spriteNum = 1; // Added sprite number tracking
+
+    public Player(GamePanel gp, KeyHandler keyH) { // Corrected parameter type
+        this.gp = gp;
+        this.keyH = keyH;
+        setDefaultValues();
+        getPlayerImage();
+    }
+
+    public void getPlayerImage() {
+        try {
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_1.png"))); // Fixed up image
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_up_2.png"))); // Fixed up image
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_1.png")));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_down_2.png")));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_1.png")));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_left_2.png")));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_1.png")));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/boy_right_2.png")));
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void setDefaultValues() {
+        x = 100;
+        y = 100;
+        speed = 4;
+        direction = "down";
+    }
+
+    public void update() {
+        if (keyH.upPressed || keyH.downPressed || keyH.leftPressed || keyH.rightPressed) {
+            // Implement sprite animation
+            spriteCounter++;
+            if (spriteCounter > 10) { // Adjust this value to control animation speed
+                spriteNum = (spriteNum == 1) ? 2 : 1;
+                spriteCounter = 0;
+            }
+
+            // Update direction and position
+            if (keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            } else if (keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            } else if (keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            } else if (keyH.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
+        }
+    }
+
+    public void draw(Graphics2D g2) {
+        BufferedImage image = null;
+
+        switch (direction) {
+            case "up":
+                image = (spriteNum == 1) ? up1 : up2;
+                break;
+            case "down":
+                image = (spriteNum == 1) ? down1 : down2;
+                break;
+            case "left":
+                image = (spriteNum == 1) ? left1 : left2;
+                break;
+            case "right":
+                image = (spriteNum == 1) ? right1 : right2;
+                break;
+        }
+
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+}
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,14 +1,15 @@
 package main;
 
+import entity.Player;
+
 import javax.swing.*;
 import java.awt.*;
-import java.sql.SQLOutput;
 
 public class GamePanel extends JPanel implements  Runnable {
     final int originalTieSize = 16;
     final int scale = 3;
 
-    final int tileSize = originalTieSize * scale;
+    public final int tileSize = originalTieSize * scale;
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
     final int screenWidth = tileSize * maxScreenCol;
@@ -16,8 +17,9 @@ public class GamePanel extends JPanel implements  Runnable {
 
     int FPS = 60;
 
-    KeyHanlder keyH = new KeyHanlder();
+    KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
 
     int playerX = 100;
     int playerY = 100;
@@ -38,41 +40,6 @@ public class GamePanel extends JPanel implements  Runnable {
     }
 
     @Override
-//    public void run() {
-//        double drawInterval = 1000000000/FPS;
-//        double v = System.nanoTime() + drawInterval;
-//        long lastTime = System.nanoTime();
-//        long timer = 0;
-//        long drawCount = 0;
-//        while(gameThread != null) {
-//            timer += (long) (v - lastTime);
-//
-//            update();
-//            repaint();
-//            drawCount++;
-//
-//            if(timer >= 1000000000) {
-//                System.out.println("FPS:" + drawCount);
-//                drawCount = 0;
-//                timer = 0;
-//            }
-//
-//
-//            try {
-//                double remainingTime = v - System.nanoTime();
-//                remainingTime = remainingTime/1000000;
-//                if(remainingTime < 0) {
-//                    remainingTime = 0;
-//                }
-//                Thread.sleep((long)remainingTime);
-//
-//                v += drawInterval;
-//            } catch (InterruptedException e) {
-//                throw new RuntimeException(e);
-//            }
-//        }
-//    }
-
     public void run() {
         double drawInterval = 1000000000/FPS;
         double delta = 0;
@@ -104,16 +71,7 @@ public class GamePanel extends JPanel implements  Runnable {
     }
 
     public void update() {
-        if(keyH.upPressed) {
-            playerY -= playerSpeed;
-        } else if(keyH.downPressed) {
-            playerY += playerSpeed;
-        } else if(keyH.leftPressed) {
-            playerX -= playerSpeed;
-        } else if(keyH.rightPressed) {
-            playerX += playerSpeed;
-        }
-
+        player.update();
     }
 
     public void paintComponent(Graphics g) {
@@ -121,10 +79,7 @@ public class GamePanel extends JPanel implements  Runnable {
 
         Graphics2D g2 = (Graphics2D) g;
 
-        g2.setColor(Color.white);
-
-        g2.fillRect(playerX, playerY, tileSize, tileSize);
-
+        player.draw(g2);
         g2.dispose();
     }
 }
